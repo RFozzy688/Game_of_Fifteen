711a26b270f14cf44bcc64fd9e200e6d27c504ba
RFozzy688 <pokerfozzy@gmail.com>
2023-05-18 05:55:06 +0300

    class Puzzle

---
+++ Game_of_Fifteen/Puzzle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Fifteen
+{
+    internal class Puzzle
+    {
+        private int _sizeField;
+        private int _methodShuffle;
+    }
+}
